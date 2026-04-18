aefda617515d67848fdbe230567b6ba8dd4efd18
Sandushi Dissanayake <sandushiumayanga1916@gmail.com>
2026-04-18 20:49:42 +0530

    updated

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,10 +1,6 @@
 const serverless = require('serverless-http');
-const express = require('express');
 const app = require('../../src/app');
 
-// Wrap the app to strip the function path prefix
-const handler = serverless(app, {
+module.exports.handler = serverless(app, {
   basePath: '/.netlify/functions/api',
 });
-
-module.exports.handler = handler;
